2304ff2c35ffe056c6eca91e00f00520441c8e97
Waris Ali <m.warisali2@gmail.com>
2018-02-22 21:33:31 +0500

    docs: Add Comments

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from read_data import read, show
 
+# Reading training and testing features from dataset
 features_labels, features_train = read(dataset="training")
 labels_test, features_test = read(dataset="testing")
 
 # Reshaping features_train and features_test as sklearn
 # requires 2D array in fit()
-
 nsamples, nx, ny = features_train.shape
 features_train = features_train.reshape((nsamples,nx*ny))
 
 nsamples, nx, ny = features_test.shape
 features_test = features_test.reshape((nsamples,nx*ny))
 
+# NaiveBayes Model
 from sklearn.naive_bayes import GaussianNB
 
 clf = GaussianNB()
@@ -22,4 +23,4 @@ labels_pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
 
-print(accuracy_score(labels_test, labels_pred))
+print("NB: ", accuracy_score(labels_test, labels_pred))
