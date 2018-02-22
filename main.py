903d0707ce40a0940130e0ccc0a9f830ad72a72e
Waris Ali <m.warisali2@gmail.com>
2018-02-22 21:13:51 +0500

    feat: Add NaiveBaye Model

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from read_data import read, show
 
 features_labels, features_train = read(dataset="training")
-test_labels, test_train = read(dataset="testing")
+labels_test, features_test = read(dataset="testing")
+
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+
+clf.fit(features_train, features_labels)
+
+labels_pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(labels_test, labels_pred))
