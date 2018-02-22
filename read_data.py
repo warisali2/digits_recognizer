15123ac6393255f5015ca4c6373fd56340966156
Waris Ali <m.warisali2@gmail.com>
2018-02-22 21:15:16 +0500

    fix: Path names for testing data

--- read_data.py
+++ read_data.py
@@ -18,8 +18,8 @@ def read(dataset = "training", path = "."):
         fname_img = os.path.join(path, 'train-images.idx3-ubyte')
         fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
     elif dataset is "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
     else:
         raise ValueError("dataset must be 'testing' or 'training'")
 
